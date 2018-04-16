6fdf7581bf1be7726251144cf294ec471d728b19
Prokuma <prokuma@prokuma.kr>
2018-04-16 14:24:59 +0900

    レイアウト変更

--- twitter_bot.py
+++ twitter_bot.py
@@ -35,7 +35,7 @@ class Task:
         pass
 
     def noticeTimer(self):
-        print("Getting data from Internet...")
+        print("Getting notice data from Internet...")
         html_doc = urllib.request.urlopen(link_notice).read()
         soup = BeautifulSoup(html_doc,'html.parser')
         title = soup.find(class_="record-value-7")
@@ -44,15 +44,17 @@ class Task:
         date = soup.find(class_="record-value-4").text
         faculty = soup.find(class_="record-value-109").text
         grade = soup.find(class_="record-value-111").text
+        if grade == "":
+            grade = "全体"
         link = "https://db.jimu.kyutech.ac.jp/cgi-bin/cbdb/db.cgi?page=DBRecord&did=357&qid=all&vid=24&rid=" + post_id
         if post_id != self.previous_notice_id:
-            post = "#Prokumaの掲示板BOT\n" + "お知らせ（学生向け） No." + str(post_id) + "\n" + str(title_text) + "\n投稿日：" + str(date) + "\n対象：" + str(faculty) + str(grade) + "\n" + str(link)
+            post = "#Prokumaの掲示板BOT\n" + "お知らせ（学生向け） No." + str(post_id) + "\n" + str(title_text) + "\n日付：" + str(date) + "\n対象学科：" + str(faculty) + "\n対象学年：" + str(grade) + "\n" + str(link)
             status = api.PostUpdate(post)
             self.previous_notice_id = post_id
         threading.Timer(self.refresh_time, self.noticeTimer).start()
 
     def timetableTimer(self):
-        print("Getting data from Internet...")
+        print("Getting timetable data from Internet...")
         html_doc = urllib.request.urlopen(link_modify_timetable).read()
         soup = BeautifulSoup(html_doc,'html.parser')
         title = soup.find(class_="record-value-116")
@@ -69,13 +71,13 @@ class Task:
         grade = soup.find(class_="record-value-109").text
         link = "https://db.jimu.kyutech.ac.jp/cgi-bin/cbdb/db.cgi?page=DBRecord&did=391&qid=all&vid=24&rid=" + post_id
         if post_id != self.previous_timetable_id:
-            post = "#Prokumaの掲示板BOT\n" + "時間割・講義室変更 No." + str(post_id) + "\n" + str(title_text) + "\n科目：" + str(subject) + "\n担当教員：" + str(professor) + "\n変更日：" + str(date) + str(time) + "\n対象：" + str(faculty) + str(grade) + "\n変更以前：" + str(before) + "\n変更以降：" + str(after) + "\n" + str(link)
+            post = "#Prokumaの掲示板BOT\n" + "時間割・講義室変更 No." + str(post_id) + "\n" + str(title_text) + "\n科目：" + str(subject) + "\n担当教員：" + str(professor) + "\n日付：" + str(date) + "\n時限：" + str(time) + "\n対象学年：" + str(faculty) + "\n対象学科：" + str(grade) + "\n変更以前：" + str(before) + "\n変更以降：" + str(after) + "\n" + str(link)
             status = api.PostUpdate(post)
             self.previous_timetable_id = post_id
         threading.Timer(self.refresh_time, self.timetableTimer).start()
 
     def nolecTimer(self):
-        print("Getting data from Internet...")
+        print("Getting no leacture data from Internet...")
         html_doc = urllib.request.urlopen(link_nolec).read()
         soup = BeautifulSoup(html_doc,'html.parser')
         title = soup.find(class_="record-value-7")
@@ -88,13 +90,13 @@ class Task:
         grade = soup.find(class_="record-value-111").text
         link = "https://db.jimu.kyutech.ac.jp/cgi-bin/cbdb/db.cgi?page=DBRecord&did=361&qid=all&vid=24&rid=" + post_id
         if post_id != self.previous_nolec_id:
-            post = "#Prokumaの掲示板BOT\n" + "休講通知 No." + str(post_id) + "\n科目：" + str(title_text) + "\n担当教員：" + str(professor) + "\n休講日：" + str(date) + str(time) + "\n対象：" + str(faculty) + str(grade) + "\n" + str(link)
+            post = "#Prokumaの掲示板BOT\n" + "休講通知 No." + str(post_id) + "\n科目：" + str(title_text) + "\n担当教員：" + str(professor) + "\n日付：" + str(date) + "\n時限：" + str(time) + "\n対象学科：" + str(faculty) + "\n対象学年：" + str(grade) + "\n" + str(link)
             status = api.PostUpdate(post)
             self.previous_nolec_id = post_id
         threading.Timer(self.refresh_time, self.nolecTimer).start()
 
     def addlecTimer(self):
-        print("Getting data from Internet...")
+        print("Getting add lecture data from Internet...")
         html_doc = urllib.request.urlopen(link_addlec).read()
         soup = BeautifulSoup(html_doc,'html.parser')
         title = soup.find(class_="record-value-7")
@@ -107,13 +109,13 @@ class Task:
         grade = soup.find(class_="record-value-111").text
         link = "https://db.jimu.kyutech.ac.jp/cgi-bin/cbdb/db.cgi?page=DBRecord&did=363&qid=all&vid=24&rid=" + post_id + "&Head=&hid=&sid=n&rev=0&ssid=3-2490-3953-g31"
         if post_id != self.previous_addlec_id:
-            post = "#Prokumaの掲示板BOT\n" + "補講通知 No." + str(post_id) + "\n科目：" + str(title_text) + "\n担当教員：" + str(professor) + "\n補講日：" + str(date) + str(time) + "\n対象：" + str(faculty) + str(grade) + "\n" + str(link)
+            post = "#Prokumaの掲示板BOT\n" + "補講通知 No." + str(post_id) + "\n科目：" + str(title_text) + "\n担当教員：" + str(professor) + "\n日付：" + str(date) + "\n時限：" + str(time) + "\n対象学科：" + str(faculty) + "\n対象学年：" + str(grade) + "\n" + str(link)
             status = api.PostUpdate(post)
             self.previous_addlec_id = post_id
         threading.Timer(self.refresh_time, self.addlecTimer).start()
 
     def callTimer(self):
-        print("Getting data from Internet...")
+        print("Getting student calling data from Internet...")
         html_doc = urllib.request.urlopen(link_call).read()
         soup = BeautifulSoup(html_doc,'html.parser')
         title = soup.find(class_="record-value-7")
@@ -124,13 +126,13 @@ class Task:
         grade = soup.find(class_="record-value-119").text
         link = "https://db.jimu.kyutech.ac.jp/cgi-bin/cbdb/db.cgi?page=DBRecord&did=393&qid=all&vid=24&rid=" + post_id
         if post_id != self.previous_call_id:
-            post = "#Prokumaの掲示板BOT\n" + "学生呼出 No." + str(post_id) + "\n作件：" + str(title_text) + "\n学科：" + str(faculty) + "\n投稿日：" + str(date) + "\n対象：" + str(grade) + "\n" + str(link)
+            post = "#Prokumaの掲示板BOT\n" + "学生呼出 No." + str(post_id) + "\n作名：" + str(title_text) + "\n対象学科等：" + str(faculty) + "\n日付：" + str(date) + "\n対象学年：" + str(grade) + "\n" + str(link)
             status = api.PostUpdate(post)
             self.previous_call_id = post_id
         threading.Timer(self.refresh_time, self.callTimer).start()
         
     def scholarshipTimer(self):
-        print("Getting data from Internet...")
+        print("Getting scholarship data from Internet...")
         html_doc = urllib.request.urlopen(link_scholarship).read()
         soup = BeautifulSoup(html_doc,'html.parser')
         title = soup.find(class_="record-value-7")
@@ -145,7 +147,7 @@ class Task:
         threading.Timer(self.refresh_time, self.scholarshipTimer).start()
 
     def concentrationTimer(self):
-        print("Getting data from Internet...")
+        print("Getting concentrate lecture data from Internet...")
         html_doc = urllib.request.urlopen(link_concentration).read()
         soup = BeautifulSoup(html_doc,'html.parser')
         title = soup.find(class_="record-value-7")
@@ -157,10 +159,10 @@ class Task:
             post = "#Prokumaの掲示板BOT\n" + "集中講義 No." + str(post_id) + "\n" + str(title_text) + "\n日付：" + str(date) + "\n" + str(link)
             status = api.PostUpdate(post)
             self.previous_scholarship_id = post_id
-        threading.Timer(self.refresh_time, self.scholarshipTimer).start()
+        threading.Timer(self.refresh_time, self.concentrationTimer).start()
 
     def aboardTimer(self):
-        print("Getting data from Internet...")
+        print("Getting studying aboard data from Internet...")
         html_doc = urllib.request.urlopen(link_aboard).read()
         soup = BeautifulSoup(html_doc,'html.parser')
         title = soup.find(class_="record-value-7")
@@ -172,7 +174,7 @@ class Task:
             post = "#Prokumaの掲示板BOT\n" + "留学・国際関連 No." + str(post_id) + "\n" +  str(title_text) + "\n担当部署：" + str(charge) + "\n" + str(link)
             status = api.PostUpdate(post)
             self.previous_scholarship_id = post_id
-        threading.Timer(self.refresh_time, self.scholarshipTimer).start()
+        threading.Timer(self.refresh_time, self.aboardTimer).start()
 
 
 def main():
